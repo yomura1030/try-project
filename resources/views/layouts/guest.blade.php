24ff639cb2875d807135edb0e7c44431392fd773
yomura <yomura@bulletgroup.jp>
2023-10-29 22:05:15 +0900

    [Update] ログイン画面のロゴを変更

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-800">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{asset('images/temporary.logo.svg')}}" class="w-40 h-40 fill-current text-gray-500" />
                 </a>
             </div>
 
